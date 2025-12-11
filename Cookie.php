121d0c670891d37b8eccaef6403eccde8668f1a4
oven-ttta <sitthisak2007.1@gmail.com>
2025-12-11 15:15:17 +0700

    feat: Implement cookie login redirection, logout functionality, and session display name initialization.

--- Cookie.php
+++ Cookie.php
@@ -1,16 +1,30 @@
 <?php
     if (isset($_COOKIE['username'])) {
-        $displayName = $_COOKIE['username'];
+        $name = $_COOKIE['username'];
     } else {
-        $displayName = "Guest";
+       header("Location: Form_Cookie.php");
+       exit();
+    }
+    
+    if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+        if (isset($_POST['logout'])) {
+            setcookie("username", "", time() - 3600);
+            header("Location: Form_Cookie.php");
+            exit();
+        }
     }
 ?>
 <!DOCTYPE html>
 <html>
 <head>
-    <title>Cookie Demo</title>
+    <title>Cookie Demo</title>  
 </head>
 <body>
-    <?php echo "Name: " . $displayName . "<br>"; ?>
+    
+    <?php echo "Name: " . $name ?>
+    <form action="<?php echo $_SERVER['PHP_SELF']; ?>" method="post">
+        <input type="submit" value="Logout" name="logout">
+    </form>
+
 </body>
 </html>
--- session.php
+++ session.php
@@ -1,6 +1,10 @@
 <?php
 session_start();
-
+    if (isset($_SESSION['username'])) {
+        $displayName = $_SESSION['username'];
+    } else {
+        $displayName = "Guest";
+    }
 ?>  
 <!DOCTYPE html>
 <html>
