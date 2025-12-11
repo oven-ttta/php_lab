c3351d5065553daaf87147d0ab8d2d978ca136e0
oven-ttta <sitthisak2007.1@gmail.com>
2025-12-11 13:07:58 +0700

    Add session management features to session.php with reset and destroy options

--- session.php
+++ session.php
@@ -21,6 +21,20 @@ echo "Views = " . $_SESSION['views'];
     </p>
     <br>
     <a href="Session.php">Refresh Page</a> | 
-    <a href="Form.php">Back to Form</a>
+    <a href="Form.php">Back to Form</a> |
+    <a href="?reset=views">Reset Views</a> |
+    <a href="?destroy=session">Destroy Session</a>
 </body>
 </html>
+
+<?php
+if(isset($_GET['reset']) && $_GET['reset'] == 'views') {
+    unset($_SESSION['views']);
+    header("Location: Session.php");
+}
+
+if(isset($_GET['destroy']) && $_GET['destroy'] == 'session') {
+    session_destroy();
+    header("Location: Session.php");
+}
+?>
